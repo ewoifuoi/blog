540497898d479711cd089209fd97017d1929e476
arum1kuy1.outlook.com <arum1kuy1@outlook.com>
2025-05-21 20:15:56 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-"use client"
+
+import LatestPosts from "@/components/Home/latest-posts";
+
 // import getMDXFiles from "@/lib/utils"
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
-      <h1>test</h1>
-      <button className="w-20 h-10 m-4 bg-blue-300 text-center items-center rounded-2xl" 
-      onClick={()=>{
-
-        alert("test");
-
-      }}>test</button>
-    </div>
+    <>
+      <LatestPosts/>
+    </>
   );
 }
--- src/components/Home/latest-posts.tsx
+++ src/components/Home/latest-posts.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
-import { getBlogPosts } from '../../../posts/utils'
+import { getBlogPosts } from '@/lib/getPosts'
+import Link from 'next/link';
 
 export default function LatestPosts() {
     let latestPosts = getBlogPosts();
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+    <>
+      <h1>最近更新</h1>
+      {latestPosts
+        .sort((a,b)=>{
+          if(
+            new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)
+          ) {
+            return -1;
+          }
+          return 1;
+        })
+        .map((post) => (
+          <article key={post.slug} className='text-wrap max-w-md my-10'>
+            <Link href={"#"}>
+              <h3 className='font-bold py-2 leading-3'>{post.metadata.title}</h3>  
+            </Link>
+          </article>
+        ))}
+    </>
+  );
 }
