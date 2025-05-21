60b7a1c0d039085d2a6f7b15300e301e180d1bf1
arum1kuy1.outlook.com <arum1kuy1@outlook.com>
2025-05-21 16:08:49 +0800

    update

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,31 @@
 
-// import path from "path";
-// import matter from "gray-matter";
-// import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import fs from "fs";
 
-// export default function getMDXFiles(dir: string) {
-//     return fs.readdirSync(dir).filter((file) => path.extname(file) === ".md");
-// }
+function getMDFiles(dir: string) {
+    return fs.readdirSync(dir).filter((file) => path.extname(file) === ".md");
+}
+
+function readMDFiles(filepath: string) {
+    let rawContent = fs.readFileSync(filepath, "utf-8");
+    return matter(rawContent);
+}
+
+function getMDData(dir: string) {
+    let mdFiles = getMDFiles(dir);
+    return mdFiles.map((file)=> {
+        let {data: metadata, content } = readMDFiles(path.join(dir, file));
+        let slug = path.basename(file, path.extname(file));
+
+        return {
+            metadata,
+            slug,
+            content,
+        };
+    });
+}
+
+export function getBlogPosts() {
+    return getMDData(path.join(process.cwd(), "posts"));
+}
